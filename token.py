9303a5b24e9a068a75ab0c54dae1d8210cc16f20
Leo Trujillo Perez <leotrujillo7@hotmail.com>
2023-03-02 12:07:21 -0600

    Create token to be able to add the contact

---
+++ token.py
@@ -0,0 +1,19 @@
+import requests
+
+# Se setean los parametros a mandar al metodo response, url y params, que tiene las keys brindadas previamente para la respuesta API
+url = "https://accounts.zoho.com/oauth/v2/token"
+params = {
+    "grant_type": "refresh_token",
+    "client_id": "1000.NASSTGTS7A3YMCDXB7U7ZG09GKEN3H",
+    "client_secret": "944f75f79aef9568bc25ad6995b9cbe18506a807e2",
+    "refresh_token": "1000.365884a88476be0720d11c4931a3c026.ea403257d059d1f6727a715d72da4b9d"
+}
+
+# Make the request to obtain an access token
+resp = requests.post(url, params=params)
+
+# Extract the access token from the response
+access_token = resp.json()["access_token"]
+
+# Print the access token value
+print("Access Token:", access_token)
