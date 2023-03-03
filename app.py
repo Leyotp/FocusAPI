7a5023f0e573fda062167dba83b7df6bb8420264
Leo Trujillo Perez <leotrujillo7@hotmail.com>
2023-03-03 12:44:14 -0600

    created a method and added inputs for the contact creation

--- app.py
+++ app.py
@@ -1,28 +1,44 @@
 import requests
 import access_token
 
-at = access_token.at
+def create_contact(first_name,last_name):
+    at = access_token.at
 
-
-url = "https://www.zohoapis.com/crm/v2/Contacts"
-headers = {
-    "Authorization": f"Bearer {at}",
-    "Content-Type": "application/json"
-}
-
-# Se setea una variable data con la informacion que se enviara por campo
-data = {
-    "data": [
-        {
-            "First_Name": "Leon",
-            "Last_Name": "Trujillo"
+    try:
+        headers = {
+            "Authorization": f"Bearer {at}",
+            "Content-Type": "application/json"
         }
-    ]
-}
+
+        contact_data = {
+        "data": [
+            {
+                "First_Name": first_name,
+                "Last_Name": last_name
+            }
+        ]
+    }
+
+        url = "https://www.zohoapis.com/crm/v2/Contacts"
+
+        resp = requests.post(url, headers=headers, json=contact_data)
+        resp.raise_for_status()
+        
+
+        print("Status Code:", resp.status_code)
+        print("Contact created successfully:", resp.json())
+    
+
+    except requests.exceptions.RequestException as e:   
+        print("An error occurred while creating the contact:", str(e))
+        print(resp.text)
 
 
-response = requests.post(url, headers=headers, json=data)
 
+if __name__ == "__main__":
 
-print("Status Code:", response.status_code)
-print("Response Body:", response.json())
+    first_name = input("Enter the contact's first name: ")
+    last_name = input("Enter the contact's last name: ")
+
+    
+    create_contact(first_name, last_name)
